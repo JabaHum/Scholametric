cba51f474336a0852de0fce324ccd25643730957
JabaHum <jabajnan8@gmail.com>
2019-05-02 10:09:16 +0300

    Commit With Java Activity

--- app/src/main/java/com/example/scholametric/MainActivity.java
+++ app/src/main/java/com/example/scholametric/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.scholametric;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final EditText usr = findViewById(R.id.username);
+        final EditText pwd = findViewById(R.id.password);
+        Button login_btn = findViewById(R.id.login_button);
+
+        login_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (usr.getText().toString().equals("Admin")&&
+                pwd.getText().toString().equals("password")){
+                    Toast.makeText(getApplicationContext(),
+                            "Redirecting...", Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(getApplicationContext(),
+                            "Wrong Credentials", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
     }
 }
