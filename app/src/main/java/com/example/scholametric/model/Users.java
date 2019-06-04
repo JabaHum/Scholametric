429a64d933bb1911cf61836887e450fe2bd043b0
JabaHum <jabajnan8@gmail.com>
2019-06-04 10:34:53 +0300

    Edited Users Class

--- app/src/main/java/com/example/scholametric/model/Users.java
+++ app/src/main/java/com/example/scholametric/model/Users.java
@@ -1,19 +1,42 @@
 package com.example.scholametric.model;
 
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
 public class Users {
-    private int userid;
+
+    @SerializedName("userid")
+    @Expose
+    private String userid;
+    @SerializedName("username")
+    @Expose
     private String username;
+    @SerializedName("password")
+    @Expose
     private String password;
 
-    public int getUserid() {
+    public String getUserid() {
         return userid;
+    }
+
+    public void setUserid(String userid) {
+        this.userid = userid;
     }
 
     public String getUsername() {
         return username;
     }
 
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
     public String getPassword() {
         return password;
     }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
 }
