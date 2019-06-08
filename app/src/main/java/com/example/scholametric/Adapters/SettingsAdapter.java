7b79342b5b50dbcc1532fd90e527c99d2f9d17d1
JabaHum <jabajnan8@gmail.com>
2019-06-08 19:46:42 +0300

    Added General Info Activities and Edited the Settings adapter and added onclick listener.Edited also Schedules activites.

--- app/src/main/java/com/example/scholametric/Adapters/SettingsAdapter.java
+++ app/src/main/java/com/example/scholametric/Adapters/SettingsAdapter.java
@@ -1,6 +1,7 @@
 package com.example.scholametric.Adapters;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.RecyclerView.ViewHolder;
@@ -10,7 +11,19 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.example.scholametric.AssignClass;
+import com.example.scholametric.DBbackup;
+import com.example.scholametric.Dormitory;
+import com.example.scholametric.Exams;
+import com.example.scholametric.ExtraCurricular;
+import com.example.scholametric.Finances;
+import com.example.scholametric.GeneralInfo;
+import com.example.scholametric.Health;
+import com.example.scholametric.Notifications;
+import com.example.scholametric.Permissions;
 import com.example.scholametric.R;
+import com.example.scholametric.SetCalender;
+import com.example.scholametric.Subjects;
 
 import java.util.ArrayList;
 
@@ -31,9 +44,65 @@ public class SettingsAdapter  extends RecyclerView.Adapter{
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
+    public void onBindViewHolder(@NonNull final ViewHolder viewHolder, int i) {
         MyViewHolder.textView.setText((String) mData.get(i));
         MyViewHolder.imageView.setImageResource((Integer) mImages.get(i));
+        viewHolder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch (viewHolder.getAdapterPosition()){
+                    case 0:
+                        Intent generalinfo = new Intent(v.getContext(), GeneralInfo.class);
+                        v.getContext().startActivity(generalinfo);
+                        break;
+                    case 1:
+                        Intent DbBackup = new Intent(v.getContext(), DBbackup.class);
+                        v.getContext().startActivity(DbBackup);
+                        break;
+                    case 2:
+                        Intent permissions = new Intent(v.getContext(), Permissions.class);
+                        v.getContext().startActivity(permissions);
+                        break;
+                    case 3:
+                        Intent assignclass = new Intent(v.getContext(), AssignClass.class);
+                        v.getContext().startActivity(assignclass);
+                        break;
+                    case 4:
+                        Intent dorm = new Intent(v.getContext(), Dormitory.class);
+                        v.getContext().startActivity(dorm);
+                        break;
+                    case 5:
+                        Intent notification = new Intent(v.getContext(), Notifications.class);
+                        v.getContext().startActivity(notification);
+                        break;
+                    case 6:
+                        Intent exams = new Intent(v.getContext(), Exams.class);
+                        v.getContext().startActivity(exams);
+                        break;
+                    case 7:
+                        Intent extra = new Intent(v.getContext(), ExtraCurricular.class);
+                        v.getContext().startActivity(extra);
+                        break;
+                    case 8:
+                        Intent finance = new Intent(v.getContext(), Finances.class);
+                        v.getContext().startActivity(finance);
+                        break;
+                    case 9:
+                        Intent health = new Intent(v.getContext(), Health.class);
+                        v.getContext().startActivity(health);
+                        break;
+                    case 10:
+                        Intent subjects = new Intent(v.getContext(), Subjects.class);
+                        v.getContext().startActivity(subjects);
+                        break;
+                    case 11:
+                        Intent calender = new Intent(v.getContext(), SetCalender.class);
+                        v.getContext().startActivity(calender);
+                        break;
+
+                }
+            }
+        });
 
     }
 
